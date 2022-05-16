51c80fa87874b982fbd71565bd9a704314753141
Santiago Santos <seba00001s@gmail.com>
2022-05-16 20:33:05 -0300

    Borro mi proyecto

--- ProyectoReservApp/src/main/java/Grupo4/ReservApp/Repositorios/FotoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package Grupo4.ReservApp.Repositorios;
-
-import Grupo4.ReservApp.Entidades.Foto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FotoRepositorio extends JpaRepository<Foto, String> {
-    
-}
